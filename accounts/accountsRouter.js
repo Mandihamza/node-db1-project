3c40e42e4e72b7ccae0e8825b61ad0b5e4389e57
mandihamza <mandihamza@gmail.com>
2020-02-29 10:43:18 -0700

    Adds get routes for accounts

--- accounts/accountsRouter.js
+++ accounts/accountsRouter.js
@@ -3,4 +3,22 @@ const db = require("../data/dbConfig.js")
 
 const router = express.Router()
 
+router.get("/", async (req, res, next) => {
+    try {
+        const accounts = await db.select("*").from("accounts")
+        res.json(accounts)
+    } catch (err) {
+        next(err)
+    }
+})
+
+router.get("/:id", async (req, res, next) => {
+    try {
+        const accounts = await db.first("*").from("accounts").where({ id: req.params.id })
+        res.json(accounts)
+    } catch (err) {
+        next(err)
+    }
+})
+
 module.exports = router
