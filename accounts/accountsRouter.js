d86142c25ee8731f2102e2a1e9d4b7608fd0a53f
mandihamza <mandihamza@gmail.com>
2020-02-29 10:57:36 -0700

    Adds delete route for accounts

--- accounts/accountsRouter.js
+++ accounts/accountsRouter.js
@@ -49,4 +49,15 @@ router.put("/:id", async (req, res, next) => {
     }
 })
 
+router.delete("/:id", async (req, res, next) => {
+	try {
+		await db("accounts").where("id", req.params.id).del()
+
+        res.status(204).end()
+        console.log("Account deleted")
+	} catch(err) {
+		next(err)
+	}
+})
+
 module.exports = router
